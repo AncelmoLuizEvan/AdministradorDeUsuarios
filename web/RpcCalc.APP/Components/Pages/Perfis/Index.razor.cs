3a4d1e1e3442c9d0e5d13fad19a6b565a0cb06fb
ancelmoluizevan <ancelmo.luiz.evan@hotmail.com>
2023-11-26 02:52:01 -0400

    Troca do projeto WEB para RazorServer

---
+++ web/RpcCalc.APP/Components/Pages/Perfis/Index.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using RpcCalc.APP.Interop.Perfis;
+using RpcCalc.APP.Services.Perfis;
+
+namespace RpcCalc.APP.Components.Pages.Perfis
+{
+    public partial class Index
+    {
+        [Inject]
+        private IPerfilService Service { get; set; } = null!;
+
+        public IEnumerable<PerfilDto>? Perfis { get; set; } = new List<PerfilDto>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            var perfis = await Service.ObterTodos();
+
+            if (perfis is not null && perfis.Any())
+                Perfis = perfis;
+            else
+                Perfis = null;
+        }
+    }
+}
---
+++ web/RpcCalc.APP/Components/Pages/Usuarios/Update.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using RpcCalc.APP.Interop.Usuarios;
+using RpcCalc.APP.Mappers;
+using RpcCalc.APP.Services.Usuarios;
+
+namespace RpcCalc.APP.Components.Pages.Usuarios
+{
+    public partial class Update
+    {
+        [Inject]
+        private IUsuarioService Service { get; set; } = null!;
+        [Inject]
+        private NavigationManager Navigation { get; set; } = null!;
+        protected UsuarioViewModel Usuario { get; set; } = new UsuarioViewModel();
+
+        protected string Mensagem = string.Empty;
+
+        [Parameter]
+        public string Id { get; set; } = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (!string.IsNullOrEmpty(Id))
+            {
+                var usuarioId = Guid.Parse(Id);
+                var result = await Service.Capturar(usuarioId);
+
+                if (result != null)
+                    Usuario = result.DtoForViewModel();
+            }
+        }
+
+        private async Task Save()
+        {
+            var result = await Service.Alterar(Guid.Parse(Id), Usuario);
+
+            if (result is not null)
+                Navigation.NavigateTo("/usuario/list");
+            else
+                Mensagem = "Ocorreu um erro, o usuário não foi atualizado";
+        }
+    }
+}
