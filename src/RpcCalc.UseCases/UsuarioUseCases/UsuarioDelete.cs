ec5d2b98d61c6f88b51a3ed03555cefe14c14114
ancelmoluizevan <ancelmo.luiz.evan@hotmail.com>
2023-12-14 23:18:45 -0400

    início api de authenticação

--- src/RpcCalc.UseCases/UsuarioUseCases/UsuarioDelete.cs
+++ src/RpcCalc.UseCases/UsuarioUseCases/UsuarioDelete.cs
@@ -1,4 +1,5 @@
-﻿using RpcCalc.Domain.Interfaces.Repositories;
+﻿using RpcCalc.Domain.Interfaces;
+using RpcCalc.Domain.Interfaces.Repositories;
 using RpcCalc.Domain.Interfaces.RepositoriesReadOnly;
 using RpcCalc.Domain.Interfaces.UseCases.UsuarioUseCase;
 
@@ -6,16 +7,19 @@ namespace RpcCalc.UseCases.UsuarioUseCases
 {
     public class UsuarioDelete : IUsuarioDelete
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IUsuarioRepository _repository;
         private readonly IUsuarioRepositoryReadOnly _repositoryReadOnly;
         private readonly IUsuarioPerfilRepositoryReadOnly _usuarioPerfilRepositoryReadOnly;
         private readonly IUsuarioPerfilRepository _usuarioPerfilRepository;
 
-        public UsuarioDelete(IUsuarioRepository repository,
+        public UsuarioDelete(IUnitOfWork unitOfWork,
+            IUsuarioRepository repository,
             IUsuarioRepositoryReadOnly repositoryReadOnly,
             IUsuarioPerfilRepositoryReadOnly usuarioPerfilRepositoryReadOnly,
             IUsuarioPerfilRepository usuarioPerfilRepository)
         {
+            _unitOfWork = unitOfWork;
             _repository = repository;
             _repositoryReadOnly = repositoryReadOnly;
             _usuarioPerfilRepositoryReadOnly = usuarioPerfilRepositoryReadOnly;
@@ -26,16 +30,26 @@ namespace RpcCalc.UseCases.UsuarioUseCases
         {
             try
             {
+                _unitOfWork.BeginTransaction();
+
                 var result = await _repositoryReadOnly.Capturar(id);
+
+                foreach (var item in result!.UsuarioPerfis)
+                {
+                    await _usuarioPerfilRepository.Excluir(item);
+                }
 
                 if (result is not null)
                     await _repository.Excluir(result);
+
+                _unitOfWork.Commit();
 
                 return true;
             }
             catch (Exception ex)
             {
                 var logError = ex.ToString();
+                _unitOfWork.Rollback();
                 return false;
                 throw;
             }
