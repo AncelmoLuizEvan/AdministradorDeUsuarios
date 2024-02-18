f0fd2f5d333394b1e1c5ba53e159d42a7fa751e4
ancelmoluizevan <ancelmo.luiz.evan@hotmail.com>
2024-02-18 13:49:55 -0400

    correção n progran para aparecer swagger

--- src/RpcCalc.API/Program.cs
+++ src/RpcCalc.API/Program.cs
@@ -40,11 +40,10 @@ builder.Services.AddAuthentication(x =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseApiCorsConfiguration(builder.Configuration);
 
